92e62615347b7b2cdbe92c93bce5ddaa2fe2bfd5
Nihat <kaffinpx@gmail.com>
2021-07-08 15:48:09 +0300

    cool things

--- index.js
+++ index.js
@@ -1,63 +1,57 @@
-(function() {
-  const https = require('https')
-  let nodeApi = 'api-beta.banano.cc'
+const https = require('https')
+const nodeApi = 'api-beta.banano.cc'
 
-  const utils = require('./src/utils')
+const utils = require('./src/utils')
 
-  function getWalletByName (name) {
-    return new Promise((resolve, reject) => {
-        if(!name.includes('.k')) {
-          reject(new Error('INVALID_SUFFIX'))
-        }
-        name = name.replace('.k', '')
+function getWalletByName (name) {
+  return new Promise((resolve, reject) => {
+    if (!name.includes('.k')) {
+      reject(new Error('INVALID_SUFFIX'))
+    }
+    name = name.replace('.k', '')
 
-        const data = JSON.stringify({
-          action: 'account_history',
-          account: 'ban_3name8cc7cci77jggyqi68us4pagun5qf3bg3u5pfxgwitc4femsh5sua3j9',
-          count: 1,
-          reverse: true
-        })
+    const data = JSON.stringify({
+      action: 'account_history',
+      account: 'ban_3name8cc7cci77jggyqi68us4pagun5qf3bg3u5pfxgwitc4femsh5sua3j9',
+      count: 1,
+      reverse: true
+    })
 
-        const options = {
-          hostname: nodeApi,
-          port: 443,
-          path: '/',
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': data.length
-          }
-        }
+    const options = {
+      hostname: nodeApi,
+      port: 443,
+      path: '/',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': data.length
+      }
+    }
 
-        const req = https.request(options, res => {
-          res.on('data', d => {
-            const blocks = JSON.parse(d.toString())["history"]
-            blocks.forEach(block => {
-                if(!block.type === "receive") return
-                const blockData = utils.splitStringBySegmentLength(block.amount.replace('5', ''), 2)
-                let blockText = ''
-                blockData.forEach(text => {
-                  if(text === '00' || text === '0') return
-                  blockText = blockText + utils.dataToText(text)
-                })
-                if(blockText === name) { resolve(block.account) }
-                reject(new Error('CANNOT_FIND_NAME'))
-            });
+    const req = https.request(options, res => {
+      res.on('data', d => {
+        const blocks = JSON.parse(d.toString()).history
+        blocks.forEach(block => {
+          if (!block.type === 'receive') return
+          const blockData = utils.splitStringBySegmentLength(block.amount.replace('5', ''), 2)
+          let blockText = ''
+          blockData.forEach(text => {
+            if (text === '00' || text === '0') return
+            blockText = blockText + utils.dataToText(text)
           })
+          if (blockText === name) { resolve(block.account) }
+          reject(new Error('CANNOT_FIND_NAME'))
         })
-
-        req.write(data)
-        req.end()
       })
-  }
+    })
 
-  function createDataByName (name) {
-    return utils.textToData(name)
-  }
+    req.write(data)
+    req.end()
+  })
+}
 
-  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-    module.exports = { nodeApi, getWalletByName, createDataByName };
-  } else {
-    window.knames = { nodeApi, getWalletByName, createDataByName };
-  }
-})();
+function createDataByName (name) {
+  return utils.textToData(name)
+}
+
+module.exports = { nodeApi, getWalletByName, createDataByName }
--- src/utils.js
+++ src/utils.js
@@ -1,39 +1,33 @@
-(function() {
-    function splitStringBySegmentLength(source, segmentLength) {
-        if (!segmentLength || segmentLength < 1) throw new Error('Segment length must be defined and greater than/equal to 1')
-        const target = [];
-        for (
-            const array = Array.from(source);
-            array.length;
-            target.push(array.splice(0,segmentLength).join('')));
-        return target;
-    }
+function splitStringBySegmentLength (source, segmentLength) {
+  if (!segmentLength || segmentLength < 1) throw new Error('Segment length must be defined and greater than/equal to 1')
+  const target = []
+  for (
+    const array = Array.from(source);
+    array.length;
+    target.push(array.splice(0, segmentLength).join('')));
+  return target
+}
 
-    function dataToText(num) {
-        const textArray = splitStringBySegmentLength(num, 2)
-        let letters = ''
-        textArray.forEach(number => {
-            const parseNumber = number.toString()
-            if(parseNumber.startsWith(0)) { parseNumber.replace(0, '') }
-            letters = letters + String.fromCharCode(parseInt(parseNumber) + 97 - 1)
-        })
-        return letters
-    }
+function dataToText (num) {
+  const textArray = splitStringBySegmentLength(num, 2)
+  let letters = ''
+  textArray.forEach(number => {
+    const parseNumber = number.toString()
+    if (parseNumber.startsWith(0)) { parseNumber.replace(0, '') }
+    letters = letters + String.fromCharCode(parseInt(parseNumber) + 97 - 1)
+  })
+  return letters
+}
 
-    function textToData(text) {
-        const textArray = splitStringBySegmentLength(text, 1)
-        let parsedText = ''
-        textArray.forEach(letter => {
-            let parsedLetter = letter.charCodeAt(0) - 97 + 1
-            if(parsedLetter.toString().length === 1) { parsedLetter = '0' + parsedLetter.toString() }
-            parsedText = parsedText + parsedLetter
-        })
-        return parsedText
-    }
+function textToData (text) {
+  const textArray = splitStringBySegmentLength(text, 1)
+  let parsedText = ''
+  textArray.forEach(letter => {
+    let parsedLetter = letter.charCodeAt(0) - 97 + 1
+    if (parsedLetter.toString().length === 1) { parsedLetter = '0' + parsedLetter.toString() }
+    parsedText = parsedText + parsedLetter
+  })
+  return parsedText
+}
 
-    if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-        module.exports = {textToData, dataToText, splitStringBySegmentLength}
-      } else {
-        window.knames.utils = {textToData, dataToText, splitStringBySegmentLength}
-      }
-})();
+module.exports = { textToData, dataToText, splitStringBySegmentLength }
